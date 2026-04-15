ddd3fc0289cb35dc273b8867e4229fd42964ee73
Cheng8433 <17355946998@163.com>
2026-04-15 21:36:43 +0800

    可以允许局域网访问

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
 export default defineConfig({
   plugins: [
     react(),
   ],
+    server: {
+        host: '0.0.0.0', // 允许局域网访问
+    }
 })
